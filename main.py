97b9c6aa05b177a25986f5fd7fc549ba502e2494
eranp22 <106144718+eranp22@users.noreply.github.com>
2023-04-13 12:54:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 aws_key_id="AKIAIOSFODNN7EXAMPLE"
-aws_secret_key="wadasdkaljdsakljdlkwjkldjsakljd"
+aws_secret_access_key="wadasdkaljdsakljdlkwjkldjsakljd"
 print("test!!!!1")
 print("Dev Eran")
 print("Commit with master")
